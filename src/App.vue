6960cc718549bcb3c6e05f99f379fa36e7e0e90a
buff <wangshengjun@linksfield.net>
2021-06-06 14:58:58 +0800

    MainTodo及TodoItem页面及样式

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <main-header></main-header>
+    <main-todo></main-todo>
   </div>
 </template>
 
 <script>
 import "./assets/styles/global.styl"
 import MainHeader from './components/MainHeader.vue'
+import MainTodo from './components/MainTodo/MainTodo.vue'
 export default {
   name: "App",
   components: {
     //组件名：组件对象
-    MainHeader: MainHeader
+    MainHeader: MainHeader,
     //'mainHeader': () => import('./components/MainHeader.vue')
+    MainTodo
   }
 };
 </script>
