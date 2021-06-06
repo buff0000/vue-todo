02679b20b43622e6ca56741288766fd13986722f
buff <wangshengjun@linksfield.net>
2021-06-06 21:18:30 +0800

    MainFooter组件

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <main-header></main-header>
     <main-todo></main-todo>
+    <main-footer></main-footer>
   </div>
 </template>
 
@@ -9,13 +10,16 @@
 import "./assets/styles/global.styl"
 import MainHeader from './components/MainHeader.vue'
 import MainTodo from './components/MainTodo/MainTodo.vue'
+import MainFooter from './components/MainFooter.vue'
+
 export default {
   name: "App",
   components: {
     //组件名：组件对象
     MainHeader: MainHeader,
     //'mainHeader': () => import('./components/MainHeader.vue')
-    MainTodo
+    MainTodo,
+    MainFooter
   }
 };
 </script>
